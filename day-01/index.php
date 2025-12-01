04f46d747a9aa704b8280a75e06f5b6f7341dd65
Andreas Alsterholm <andreas@alsterholm.com>
2025-12-01 08:21:04 +0100

    Solve day 1

---
+++ day-01/index.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace day01;
+
+function part1(array $input) {
+    return solve($input, part: 1);
+}
+
+function part2(array $input) {
+    return solve($input, part: 2);
+}
+
+function solve($input, $part) {
+    $position = 50;
+    $zeroes = 0;
+
+    foreach ($input as $instruction) {
+        $direction = $instruction[0];
+        $clicks = (int) substr($instruction, 1);
+
+        for ($i = 0; $i < $clicks; $i++) {
+            $position = match ($direction) {
+                'L' => $position - 1,
+                'R' => $position + 1,
+            };
+
+            if ($position === 100) $position = 0;
+            if ($position === -1) $position = 99;
+
+            if ($part === 2 && $position === 0) $zeroes++;
+        }
+
+        if ($part === 1 && $position === 0) $zeroes++;
+    }
+
+    return $zeroes;
+}
---
+++ day-01/part_1.test.php
@@ -0,0 +1,11 @@
+<?php
+
+require './day-01/index.php';
+
+$data = array_map(trim(...), file(__DIR__ . '/data.txt'));
+
+$res1 = day01\part1($data);
+echo "Part 1: $res1.\n";
+
+$res2 = day01\part2($data);
+echo "Part 2: $res2.\n";
